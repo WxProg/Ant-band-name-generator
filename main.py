e2e6d09b608dd7f66d9f36a28a0e161b93863463
WxProg <101136336+WxProg@users.noreply.github.com>
2022-09-03 22:37:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello, Welcome to Ant Band Name Generator!")
+city = input("What city did you grew up in? \n")
+pet_name = input("What's the name of your pet? \n")
+print("The name of your band is " + city + " " + pet_name)
